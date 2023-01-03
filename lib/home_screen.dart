2a46c31ff33c41672c382495ba3beeb3e451f3a5
SabinSnider <sabinvsic@gmail.com>
2023-01-03 21:22:19 +0545

    model added

---
+++ lib/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body:// This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
